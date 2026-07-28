e39597d857c964f477dcafaeaecfc97066c9b85b
shorted-again <marcosreyes@cpp.edu>
2026-07-28 09:51:53 -0700

    Update CargoAircraftClass.cs
    Small changes

--- AirspaceProject/CargoAircraftClass.cs
+++ AirspaceProject/CargoAircraftClass.cs
@@ -6,7 +6,7 @@ namespace AirSimulation
 {
     public class CargoAircraft : Aircraft
     {
-        public double CargoWeight { get; set; }
+        public double CargoWeight { get; }
 
         public CargoAircraft(string flightNumber, bool isEmergency, double cargoWeight)
             : base(flightNumber, isEmergency)
@@ -19,11 +19,18 @@ namespace AirSimulation
             CargoWeight = cargoWeight;
         }
 
-        public override string AircraftType => "Cargo";
+        public override string AircraftType => "Cargo Aircraft";
 
         public override string GetDescription()
         {
-            return $"{FlightNumber} - Cargo Aircraft, Cargo Weight: {CargoWeight} lbs";
+            return $"{FlightNumber} - Cargo Aircraft, " + $"Cargo Weight: {CargoWeight} lbs";
+
+            if (IsEmergency)
+            {
+                description += ", Emergency";
+            }
+
+            return description;
         }
     }
 }
